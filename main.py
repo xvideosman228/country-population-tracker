76df6ae7ac74a27e8364c924a42d93a8c55d50d8
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-10-11 02:35:10 +0300

    Enhance error handling of the fetching function.

--- main.py
+++ main.py
@@ -5,18 +5,24 @@ page_url = "https://www.worldometers.info/world-population/population-by-country
 
 
 def fetch_page_content(url):
-
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
     }
+    try:
+        page = requests.get(url, headers=headers, timeout=5)
+        page.raise_for_status()
+        return page.content         # Return HTML content if no error occurs
     
-    page = requests.get(url, headers=headers, timeout=5)
-    if page.status_code == 200:
-        return page.content
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.Timeout as timeout_err:
+        print(f"Timeout error occurred: {timeout_err}")
+    except requests.exceptions.ConnectionError as connect_err:
+        print(f"Connection error occurred. {connect_err}")
+    except requests.exceptions.RequestException as error:
+        print(f"An error occurred: {error}")
+    
+    return None
 
-    else:
-        print("An error occurred !!")
-        
+
 fetch_page_content(page_url)
-
-
