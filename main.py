06f9add1408cf3dd89b2a61e80dc47fbffaab8fd
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-10-11 03:40:10 +0300

    Add function to parse html content using bs4.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-page_url = "https://www.worldometers.info/world-population/population-by-country/"
-
 
 def fetch_page_content(url):
     headers = {
@@ -11,7 +9,7 @@ def fetch_page_content(url):
     try:
         page = requests.get(url, headers=headers, timeout=5)
         page.raise_for_status()
-        return page.content         # Return HTML content if no error occurs
+        return page.content  # Return HTML content if no error occurs
     
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
@@ -25,4 +23,42 @@ def fetch_page_content(url):
     return None
 
 
-fetch_page_content(page_url)
+def parse_page_content(html_content):
+    soup = BeautifulSoup(html_content, 'lxml')
+    
+    # Find the table container
+    table = soup.find('table', id='example2').tbody
+    
+    # Find all rows withing the table
+    rows = table.find_all('tr')
+    
+    countries_list = []  # empty list
+    
+    # Loop through each row to get the corresponding column value
+    for row in rows:
+        dic = {}  # empty dict
+        
+        dic['Sort'] = row.find_all('td')[0].text
+        dic['Country'] = row.find_all('td')[1].text
+        dic['Population (2024)'] = row.find_all('td')[2].text.replace(',', '')
+        dic['Yearly Change'] = row.find_all('td')[3].text
+        dic['Net Change'] = row.find_all('td')[4].text
+        dic['Density (P/Km2)'] = row.find_all('td')[5].text.replace(',', '')
+        dic['Land Area (Km2)'] = row.find_all('td')[6].text.replace(',', '')
+        dic['Migrants (net)'] = row.find_all('td')[7].text.replace(',', '')
+        dic['Fert. Rate'] = row.find_all('td')[8].text
+        dic['Mid. Age'] = row.find_all('td')[9].text
+        dic['Urban Pop'] = row.find_all('td')[10].text
+        dic['World Share'] = row.find_all('td')[11].text
+        
+        countries_list.append(dic)   # Append dictionary to the list
+    
+    return countries_list            # Return the list of dictionaries
+
+
+if __name__ == '__main__':
+    page_url = "https://www.worldometers.info/world-population/population-by-country/"
+    
+    html = fetch_page_content(page_url)
+    
+    parse_page_content(html)
