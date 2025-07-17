44b36d4a056a0b5de12ad23abad16bba7ca62b13
xvideosman <xvideosman228@gmail.com>
2025-07-17 11:15:38 +0300

    Now keys are converted from Russian site (because it is working) to English

--- currentPopulationParser.py
+++ currentPopulationParser.py
@@ -1,53 +1,63 @@
 #!/usr/bin/env python3
-# source - https://quantiux.com/blog/scraping-with-requests-html/
+import json
+from time import time
 from requests_html import HTMLSession
 from datetime import datetime
-from pprint import pprint
-
-class GetWorldData:
-    def __init__(self, file, url) -> None:
-        self.file = file
-        self.url = url
-
-    def saveData(self, names, counts) -> None:
-        if not names or not counts:
-            print("No data to add!")
-            return
-
-        for name, count in zip(names, counts):
-            count_text = count.text.replace(",", "").strip()
-            name_text = name.text.replace(",", "").strip()
-            self.file.write(f"{name_text},{count_text}\n")
-            print(f"{name_text},{count_text}")
-
-        time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"Data saved to {time_now}.")
 
 
-    def run(self) -> None:
-        session = HTMLSession()
-        page = session.get(self.url)
-        page.raise_for_status()
+with open('worldometers_keys_converter.json') as keysFile:
+    key = json.load(keysFile)
+url = "https://www.worldometers.info/ru"
+file_path = "world_data.json"
 
-        try:
-            page.html.render(timeout=30)
-        except Exception as e:
-            print(f"Error while parsing the page: {e}")
-            return
 
-        counts = page.html.find('.counter-number')
-        names = page.html.find('span.counter-item')
 
-        if not counts or not names:
-            print("Unable to pick the data!")
-            return
-        self.saveData(names, counts)
+def timer_func(func):
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
+        return result
+    return wrap_func
 
-def main() -> None:
-    url = "https://www.worldometers.info/ru"
-    with open("world_data.csv", "w", encoding="utf8") as file:
-        world_data = GetWorldData(file, url)
-        world_data.run()
 
-if __name__ == "__main__":
-    main()
+def saveData(names, counts, file_path) -> None:
+    if not names or not counts:
+        print("No data to write!")
+        return
+
+    data = {}
+    for name, count in zip(names, counts):
+        count_text = count.text.replace(",", "").strip()
+        name_text = key[name.text.replace(",", "").strip()]
+        data[name_text] = count_text
+
+    with open(file_path, 'w', encoding='utf8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Data was saved to JSON-file at {time_now}.")
+
+
+@timer_func
+def run() -> None:
+    session = HTMLSession()
+    page = session.get(url)
+    page.raise_for_status()
+
+    try:
+        page.html.render(timeout=20)  # увеличенный таймаут рендеринга
+    except Exception as e:
+        print(f"Error while parsing the page: {e}")
+        return
+
+    counts = page.html.find('.counter-number')
+    names = page.html.find('span.counter-item')
+
+    if not counts or not names:
+        print("Cannot pick data!")
+        return
+    saveData(names, counts, file_path)
+
+run()
